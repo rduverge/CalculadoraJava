550f2c08316995fec2e024c479614105ffadc876
Rolando Duverge <arturo1872002@gmail.com>
2022-07-08 15:53:05 -0400

    fix code

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -7,51 +7,51 @@ public class Calculadora {
 
     public static void main(String[] args) {
         boolean estado = true;
-        while(estado){
-        Scanner leerDatos = new Scanner(System.in);
-        System.out.println("Ingrese la opcion");
-        System.out.println("Digite el numero segun la operacion");
-        System.out.println("1.Suma");
-        System.out.println("2.Resta");
-        System.out.println("3.Multiplicacion");
-        System.out.println("4.Division");
-        System.out.println("5.Raiz cuadrada");
+        while (estado) {
+            Scanner leerDatos = new Scanner(System.in);
+            System.out.println("Ingrese la opcion");
+            System.out.println("Digite el numero segun la operacion");
+            System.out.println("1.Suma");
+            System.out.println("2.Resta");
+            System.out.println("3.Multiplicacion");
+            System.out.println("4.Division");
+            System.out.println("5.Raiz cuadrada");
             System.out.println("6.Potencia");
-        System.out.println("7.Salir \n");
-        try {
-            int op = Integer.parseInt(leerDatos.nextLine());
-            switch (op) {
-                case 1:
-                    suma();
-                    break;
+            System.out.println("7.Salir \n");
+            try {
+                int op = Integer.parseInt(leerDatos.nextLine());
+                switch (op) {
+                    case 1:
+                        suma();
+                        break;
 
-                case 2:
-                    resta();
-                    break;
+                    case 2:
+                        resta();
+                        break;
 
-                case 3:
-                    multiplicacion();
-                    break;
+                    case 3:
+                        multiplicacion();
+                        break;
 
-                case 4:
-                    division();
-                    break;
-                case 5:
-                    raiz();
-                    break;
-                case 6:
-                    potencia();
-                    break;
-                case 7:
-                    salir();
-                    break;
-            }
-            }catch(Exception e){
-            System.out.println("Error: Solo se aceptan numeros");
+                    case 4:
+                        division();
+                        break;
+                    case 5:
+                        raiz();
+                        break;
+                    case 6:
+                        potencia();
+                        break;
+                    case 7:
+                        salir();
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("Error: Solo se aceptan numeros");
             }
 
         }
-        }
+    }
 
     private static void potencia() {
         try {
@@ -63,7 +63,7 @@ public class Calculadora {
             double resultado = Math.pow(primerNumero, segundoNumero);
 
             System.out.println("El resultado de la potencia es : " + resultado);
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Error: Solo se aceptan numeros");
         }
     }
@@ -76,7 +76,7 @@ public class Calculadora {
             double resultado = Math.sqrt(primerNumero);
 
             System.out.println("El resultado de la Raiz cuadrada es : " + resultado);
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Error: Solo se aceptan numeros");
         }
     }
@@ -93,16 +93,16 @@ public class Calculadora {
             System.out.println("Digite el numero dos");
             segundoNumero = Integer.parseInt(leerDatos.nextLine());
 
-            if(segundoNumero == 0){
+            if (segundoNumero == 0) {
                 System.out.println("La division entre 0 da infinito");
-            }else {
+            } else {
                 int resultado = primerNumero / segundoNumero;
                 System.out.println("El resultado de la division es : " + resultado);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Error: Solo se aceptan numeros");
         }
-}
+    }
 
     private static void resta() {
         try {
@@ -114,7 +114,7 @@ public class Calculadora {
 
             int resultado = primerNumero - segundoNumero;
             System.out.println("El resultado de la resta es : " + resultado);
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Error: Solo se aceptan numeros");
         }
     }
@@ -129,7 +129,7 @@ public class Calculadora {
 
             int resultado = primerNumero + segundoNumero;
             System.out.println("El resultado de la suma es : " + resultado);
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Error: Solo se aceptan numeros");
         }
     }
@@ -144,7 +144,7 @@ public class Calculadora {
 
             int resultado = primerNumero * segundoNumero;
             System.out.println("El resultado de la multiplicacion es : " + resultado);
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Error: Solo se aceptan numeros");
         }
     }
